b46574ef16c5bf50c4442cd2c436eb331fc8115f
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2025-03-28 22:43:12 +0000

    Multithreading_demos

---
+++ src/main/java/sequential/SequentialExecutionDemo.java
@@ -0,0 +1,22 @@
+package main.java.sequential;
+
+public class SequentialExecutionDemo {
+    public static void main(String[] args) {
+        demo1();
+        demo2();
+    }
+
+    private static void demo1() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("From demo 1 " + i);
+        }
+    }
+
+    private static void demo2() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("From demo 2 " + i);
+        }
+    }
+}
+
+// each and every program is single threaded unless otherwise specified
